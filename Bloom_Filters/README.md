f40b0653b84f6df70662d46e294a71f82d2dc1f2
Ashis Nayak <aashish.nayak@gmail.com>
2015-01-02 20:45:19 +1100

    Code clean up + Readme

---
+++ Bloom_Filters/README.md
@@ -0,0 +1,23 @@
+#Counting Code Lines
+
+This is a program written in Node.js to implement bloom filter. The problem description can be found here:
+
+http://codekata.com/kata/kata05-bloom-filters/
+
+#Running
+
+Clone the directory structure.
+Install Node.js (if not present already)
+```
+node filter.js
+```
+
+#Mention
+
+The code for liner (stream API parsing file line by line) is taken from here:
+
+http://strongloop.com/strongblog/practical-examples-of-the-new-node-js-streams-api/
+
+Readline could have been used, but not because of stability issues as mentioned in the API docs:
+
+http://nodejs.org/api/readline.html
--- Bloom_Filters/filter.js
+++ Bloom_Filters/filter.js
@@ -1,16 +1,22 @@
+/*
+ * module implementing Bloom Filter operation i.e. addWord and hasWord
+ */
 var BloomFilters = (function () {
-    var BitArray = require('node-bitarray');
+    // crypto API to perform hashing
     var crypto = require('crypto');
 
-    var _bitMap = new BitArray(2 ^ 20);
+    // JavaScript ArrayBuffer used as bit array
+    var _bitMap = new Int32Array(new ArrayBuffer(64*1024));
     var _hashAlgos = ['md5', 'sha1', 'sha256', 'sha512'];
 
+    // method to add a word to the dictionary
     var addWord = function (word) {
         _hashAlgos.forEach(function (hashAlgo) {
             _bitMap[crypto.createHash(hashAlgo).update(word).digest('binary')] = 1;
         });
     };
 
+    // method to search a word in the dictionary
     var hasWord = function (word) {
         return _hashAlgos.every(function (hashAlgo) {
             return _bitMap[crypto.createHash(hashAlgo).update(word).digest('binary')] !== undefined;
@@ -37,7 +43,7 @@ function init() {
         })
         .on('end', function () {
             console.log('Finished populating words from wordlist.txt file.');
-            console.log('Search for word "ABD": ' + (BloomFilters.search('ABD') ? 'Found' : 'Not Found'));
-            console.log('Search for word "ABD": ' + (BloomFilters.search('ABDDBA') ? 'Found' : 'Not Found'));
+            console.log('Searching for word "found": ' + (BloomFilters.search('found') ? 'Found' : 'Not Found'));
+            console.log('Searching for word "notfound": ' + (BloomFilters.search('NotFound') ? 'Found' : 'Not Found'));
         });
 }
